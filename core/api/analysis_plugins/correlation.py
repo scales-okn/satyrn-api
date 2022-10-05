1a9fbe9b8bec68ae1be522af608b24461cc61ebf
DonnaHooshmand <Donnahooshmand@yahoo.com>
2022-10-04 23:15:27 -0500

    Adding License

--- core/api/analysis_plugins/correlation.py
+++ core/api/analysis_plugins/correlation.py
@@ -1,4 +1,16 @@
 '''
+This file is part of Satyrn.
+Satyrn is free software: you can redistribute it and/or modify it under 
+the terms of the GNU General Public License as published by the Free Software Foundation, 
+either version 3 of the License, or (at your option) any later version.
+Satyrn is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Satyrn. 
+If not, see <https://www.gnu.org/licenses/>.
+'''
+
+'''
 Pending correlation development:
 - questions like where there are different filters on each of the calculations
     e.g. correlation groupby committee
