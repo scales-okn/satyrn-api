3503a38281d37f1931c0a5ec61abf748c9cb3545
Danny O'Neal <danny.e.oneal@gmail.com>
2023-08-09 14:39:10 -0700

    testing qa deploy

--- core/api/utils.py
+++ core/api/utils.py
@@ -9,7 +9,7 @@ See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with Satyrn. 
 If not, see <https://www.gnu.org/licenses/>.
 '''
-
+# testing deploy
 
 from .sql_func import _nan_cast, sql_concat
 from .operations import OPERATION_SPACE as OPS
